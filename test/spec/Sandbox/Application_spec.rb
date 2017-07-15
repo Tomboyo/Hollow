9045fd00420c8c172b4fc505dc3f9e9f5119a401
Tom Simmons <tomasimmons@gmail.com>
2017-07-14 23:43:00 -0400

    Refactor Application spec

--- test/spec/Sandbox/Application_spec.rb
+++ test/spec/Sandbox/Application_spec.rb
@@ -3,45 +3,40 @@ require_relative '../../../lib/Sandbox/Application'
 
 describe Sandbox::Application do
 
-  application = Sandbox::Application.new({
-    # Do not require any resources outside this spec
-    :autorequire => {
-      :directories => []
+  before do
+    @application = Sandbox::Application.new({
+      # Do not require any resources outside this spec
+      :autorequire => {
+        :directories => []
+      }
+    })
+  end
+
+  it 'Delegates to Resources' do
+
+    class AResource
+      extend Sandbox::Resource
+      def self.getHandler ; AResource.new ; end
+      def get(*args) ; 5 ; end
+    end
+
+    assert_equal 5, @application.handle_request(:AResource, :get)
+  end
+
+  it 'Only delegates to actual Resources' do
+    SOME_CONSTANT = 5
+    assert_raises(Sandbox::ResourceException) {
+      @application.handle_request(:SOME_CONSTANT, :get)
     }
-  })
 
-  describe 'handle_request' do
-    it 'Will forward requests to Resource-extending classes' do
+    module SomeModule ; end
+    assert_raises(Sandbox::ResourceException) {
+      @application.handle_request(:SomeModule, :get)
+    }
 
-      class AResource
-        extend Sandbox::Resource
-        def self.getHandler ; AResource.new ; end
-        def get(*args) ; 5 ; end
-      end
-
-      assert_equal 5, application.handle_request(:AResource, :get)
-    end
-
-    it 'Will not forward to non-Resource Objects' do
-      SOME_CONSTANT = 5
-      assert_raises(Sandbox::ResourceException) {
-        application.handle_request(:SOME_CONSTANT, :get)
-      }
-
-      module SomeModule ; end
-      assert_raises(Sandbox::ResourceException) {
-        application.handle_request(:SomeModule, :get)
-      }
-
-      class SomeClass ; end
-      assert_raises(Sandbox::ResourceException) {
-        application.handle_request(:SomeClass, :get)
-      }
-
-      some_instance = SomeClass.new
-      assert_raises(Sandbox::ResourceException) {
-        application.handle_request(:some_instance, :get)
-      }
-    end
+    class SomeClass ; end
+    assert_raises(Sandbox::ResourceException) {
+      @application.handle_request(:SomeClass, :get)
+    }
   end
 end
