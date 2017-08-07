211260372a839686f9e84c878c2a0967d727be07
Tom Simmons <tomasimmons@gmail.com>
2017-08-06 21:47:15 -0400

    Remove old test case

--- test/spec/Sandbox/Application_spec.rb
+++ test/spec/Sandbox/Application_spec.rb
@@ -88,27 +88,4 @@ describe Sandbox::Application do
     assert_equal "A", application.handle_request(:AResource, :get)
     assert_equal "B", application.handle_request(:BResource, :get)
   end
-
-  it 'Invokes filters before accessing Resources' do
-    class AuthenticationFilter
-      class AuthenticationError < StandardError ; end
-      def self.filter(request = {}, context = {})
-        if request[:user].nil? || request[:password].nil?
-          fail AuthenticationError, "Failed to authenticate #{request[:user]}"
-        end
-      end
-    end
-
-    class AuthenticatedResource
-      extend Sandbox::Resource
-      extend Sandbox::Filtered
-      def self.getFilters ; [ AuthenticationFilter ] ; end
-      def self.getHandler ; self.new ; end
-      def get ; return true ; end
-    end
-
-    assert_raises(AuthenticationError) do
-      @application.handle_request(:AuthenticatedResource, :get)
-    end
-  end
 end
